6364ad54dbfab437a917a11aede41bef8d7fb130
mirsahib <mirsahib24@gmail.com>
2020-08-06 13:58:34 +0600

    :wrench: Add path for passport key

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         Passport::routes();
 
-
+        Passport::loadKeysFrom('storage/');
         //
     }
 }
